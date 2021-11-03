9a005befc8d7bac645429f4b6402c4573ba6c1a6
AlbMarc <albert.k.marciniak@googlemail.com>
2021-11-03 17:56:47 +0100

    Split Files
    Split event txt files into one with all the Sleep stages and one wit all the other events.

--- SplitFiles.ps1
+++ SplitFiles.ps1
@@ -3,7 +3,7 @@ Function Get-FolderPath($title)                                                 
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 
     $FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
-        SelectedPath = 'D:\GoogleDrive\Vorlesungen'
+        SelectedPath = 'D:\Git\PSscripts'
         #SelectedPath = '\\OFFICE-SRV\shared\Projekte\SCAPIS\MainTrial'
     }
     $FolderBrowserDialog.Description = $title
@@ -14,6 +14,7 @@ Function Get-FolderPath($title)                                                 
 
 
 $path = Get-FolderPath("TeX-File")
+#$path = "D:\Git\PSscripts"
 
 if ($path -eq ""){
     break
@@ -22,6 +23,41 @@ Write-Host $path
 
 Set-Location -Path $path       
 
-$textfile = (Get-Childitem $path | Where{$_.Extension -eq ".txt"}).FullName
+$textfile = (Get-Childitem $path | Where-Object{$_.Extension -eq ".txt"}).FullName
 
-$str = Get-Content $textfile
+$textfile | ForEach-Object{
+    if ($_ -ccontains "_Sleep" -or $_ -ccontains "_Event"){
+        Write-Host "split files found"
+    } else {
+        $content = Get-Content $_
+        $fname = $_.Split(".")
+    
+        $outSleepName = $fname[0] + "_Sleep." + $fname[1]
+        $outEventsName = $fname[0] + "_Event." + $fname[1]
+
+        New-Item -Path $outSleepName -ItemType File -Force
+        New-Item -Path $outEventsName -ItemType File -Force
+
+        $splitflag = $false
+
+        $content | ForEach-Object{
+            if ($splitflag -eq $false){
+                $_ | Out-File $outSleepName -Append
+                $_ | Out-File $outEventsName -Append
+                if (($_ -match "Events Included:")-xor($_ -match "Sleep Stage")){
+                    $splitflag = $true
+                }
+            }else{
+                if ($_ -match "SLEEP"){
+                    $_ | Out-File $outSleepName -Append
+                }elseif ($_ -match "Scoring Session:") {
+                    "`n"+ $_ | Out-File $outSleepName -Append
+                    $_ | Out-File $outEventsName -Append
+                    $splitflag = $false
+                }else{
+                    $_ | Out-File $outEventsName -Append
+                }
+            }
+        }
+    }
+}
